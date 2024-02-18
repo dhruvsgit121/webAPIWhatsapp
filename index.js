606cb9963520709635e17e6d0ce0a07e29dbff79
10956155 <dhruv.sgit@gmail.com>
2024-02-18 13:36:11 +0530

    chaged files.

--- index.js
+++ index.js
@@ -1,36 +1,28 @@
-require("dotenv").config();
-
 const express = require("express");
 const body_parser = require("body-parser");
-const { default: axios } = require("axios");
+const axios = require("axios");
+require("dotenv").config();
 
 const app = express().use(body_parser.json());
 
 const token = process.env.TOKEN;
-const mytoken = process.env.MYTOKEN;
+const mytoken = "dhruv";
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-  console.log("value of token is " + token);
-  console.log("value of mytoken is " + mytoken);
-  console.log("value of port is " + port);
   console.log("Webhook is listening");
 });
 
 app.get("/webhook", (request, response) => {
-  console.log("called2");
-  console.log("request is get" + request);
-
+  console.log("webhook get request called!!!");
   let body_param = request.body;
-  
-    // console.log("request is post" + request);
-    console.log(JSON.stringify(body_param, null, 2));
+  console.log(JSON.stringify(body_param, null, 2));
 
   let mode = request.query["hub.mode"];
   let challenge = request.query["hub.challenge"];
   let token = request.query["hub.verify_token"];
 
-  // const mytoken = "";
+  // const mytoken = "dhruv";
 
   if (mode && token) {
     if (mode === "subscribe" && token === mytoken) {
@@ -41,12 +33,11 @@ app.get("/webhook", (request, response) => {
   }
 });
 
-app.post("/dhruv", (request, response) => {
-
+app.post("/webhook", (request, response) => {
   console.log("called1");
   let body_param = request.body;
 
-process.stderr.write("request is post" + request);
+  // process.stderr.write("request is post" + request);
 
   // console.log("request is post" + request);
   console.log(JSON.stringify(body_param, null, 2));
@@ -81,65 +72,62 @@ process.stderr.write("request is post" + request);
           "Content-Type": "application/json",
         },
       });
-      response.status(200);
+      response.sendStatus(200);
     } else {
-      response.status(404);
+      response.sendStatus(404);
     }
   }
 });
 
-
 app.post("/dhruvgupta", (request, response) => {
+  response.sendStatus(200).send("running post request");
 
-  response.sendStatus(200);
+  //   console.log("called1");
+  //   let body_param = request.body;
 
-//   console.log("called1");
-//   let body_param = request.body;
+  // process.stderr.write("request is post" + request);
 
-// process.stderr.write("request is post" + request);
+  //   // console.log("request is post" + request);
+  //   console.log(JSON.stringify(body_param, null, 2));
 
-//   // console.log("request is post" + request);
-//   console.log(JSON.stringify(body_param, null, 2));
+  //   if (body_param.object) {
+  //     if (
+  //       body_param.entry &&
+  //       body_param.entry[0].changes &&
+  //       body_param.entry[0].changes[0].value.messages &&
+  //       body_param.entry[0].changes[0].value.messages[0]
+  //     ) {
+  //       let phone_no_id =
+  //         body_param.entry[0].changes[0].value.metadata.phone_number_id;
+  //       let from = body_param.entry[0].changes[0].value.messages[0].from;
+  //       let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
 
-//   if (body_param.object) {
-//     if (
-//       body_param.entry &&
-//       body_param.entry[0].changes &&
-//       body_param.entry[0].changes[0].value.messages &&
-//       body_param.entry[0].changes[0].value.messages[0]
-//     ) {
-//       let phone_no_id =
-//         body_param.entry[0].changes[0].value.metadata.phone_number_id;
-//       let from = body_param.entry[0].changes[0].value.messages[0].from;
-//       let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
-
-//       axios({
-//         method: post,
-//         url:
-//           "https://graph.facebook.com/v18.0/" +
-//           phone_no_id +
-//           "/messages?access_token=" +
-//           token,
-//         data: {
-//           messaging_product: "whatsapp",
-//           to: from,
-//           text: {
-//             body: "Hi!!!!!!!!!!!!!! I am Dhruv Gupta",
-//           },
-//         },
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//       });
-//       response.status(200);
-//     } else {
-//       response.status(404);
-//     }
-//   }
+  //       axios({
+  //         method: post,
+  //         url:
+  //           "https://graph.facebook.com/v18.0/" +
+  //           phone_no_id +
+  //           "/messages?access_token=" +
+  //           token,
+  //         data: {
+  //           messaging_product: "whatsapp",
+  //           to: from,
+  //           text: {
+  //             body: "Hi!!!!!!!!!!!!!! I am Dhruv Gupta",
+  //           },
+  //         },
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //       });
+  //       response.status(200);
+  //     } else {
+  //       response.status(404);
+  //     }
+  //   }
 });
 
 app.get("/", (request, response) => {
-  
   response.status(200).send("Webhook is setup!!! Dhruv Gupta Testing the API");
 });
 
