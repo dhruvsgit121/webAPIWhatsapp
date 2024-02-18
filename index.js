002ab440dedc62c43d0afe4efc2a9b0c5c5ea68f
10956155 <dhruv.sgit@gmail.com>
2024-02-18 17:51:41 +0530

    Change for button demo/.

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ require("dotenv").config();
 const app = express().use(body_parser.json());
 
 // const token = process.env.TOKEN;
-const token = "EAAP6A94pp78BO12m5xZAkYaHxGyBYfpYuJbxsYryFV5m2QeVlq9RzTkqjri2GMCNxyt3sbhT2PWCrVZBvZAZBjdlj0Kg9QFBBUigyjVl5Lss6vVvuD0nPHZA4ZCiAWUUNW5RqOadOM0e3gjc15vSCHjafG5aGbJNStHuLzcemXxx9uPm1fNCBNWuUNrdcl9KdDBifObaQtGZCTqw8ry6IMZD";
+const token =
+  "EAAP6A94pp78BO12m5xZAkYaHxGyBYfpYuJbxsYryFV5m2QeVlq9RzTkqjri2GMCNxyt3sbhT2PWCrVZBvZAZBjdlj0Kg9QFBBUigyjVl5Lss6vVvuD0nPHZA4ZCiAWUUNW5RqOadOM0e3gjc15vSCHjafG5aGbJNStHuLzcemXxx9uPm1fNCBNWuUNrdcl9KdDBifObaQtGZCTqw8ry6IMZD";
 const mytoken = "dhruv";
 const port = process.env.PORT || 8000;
 
@@ -53,7 +54,27 @@ app.post("/webhook", (request, response) => {
       let phone_no_id =
         body_param.entry[0].changes[0].value.metadata.phone_number_id;
       let from = body_param.entry[0].changes[0].value.messages[0].from;
+
+      var msg_body = "";
+      if (body_param.entry[0].changes[0].value.messages[0].text.body) {
+        msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+      } else {
+        if (body_param.entry[0].changes[0].value.messages[0].type == "button") {
+          msg_body =
+            body_param.entry[0].changes[0].value.messages[0].button.text;
+        } else {
+          msg_body = "DEFAULT MESSAGE!!!";
+        }
+      }
       let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+
+      // if(){
+
+      // }  "type": "button",
+      //                 "button": {
+      //                   "payload": "B",
+      //                   "text": "dsfsfsf"
+      //                 }
 
       axios({
         method: "POST",
@@ -80,61 +101,102 @@ app.post("/webhook", (request, response) => {
   }
 });
 
+// {
+//   "object": "whatsapp_business_account",
+//   "entry": [
+//     {
+//       "id": "244715655388071",
+//       "changes": [
+//         {
+//           "value": {
+//             "messaging_product": "whatsapp",
+//             "metadata": {
+//               "display_phone_number": "15551291482",
+//               "phone_number_id": "224738690728648"
+//             },
+//             "contacts": [
+//               {
+//                 "profile": {
+//                   "name": "Dhruv Gupta"
+//                 },
+//                 "wa_id": "919015346166"
+//               }
+//             ],
+//             "messages": [
+//               {
+//                 "context": {
+//                   "from": "15551291482",
+//                   "id": "wamid.HBgMOTE5MDE1MzQ2MTY2FQIAERgSOTVCRTUxRDIzNTA0Rjg4RjAzAA=="
+//                 },
+//                 "from": "919015346166",
+//                 "id": "wamid.HBgMOTE5MDE1MzQ2MTY2FQIAEhggQTdCMUVFQzE3QzgwRUE3MjE0NzlDNUJBNDQ4OUUwMkMA",
+//                 "timestamp": "1708258497",
+//                 "type": "button",
+//                 "button": {
+//                   "payload": "B",
+//                   "text": "dsfsfsf"
+//                 }
+//               }
+//             ]
+//           },
+//           "field": "messages"
+//         }
+//       ]
+//     }
+//   ]
+// }
 
 app.get("/", (request, response) => {
   response.status(200).send("Webhook is setup!!! Dhruv Gupta Testing the API");
 });
 
-
 // app.post("/dhruvgupta", (request, response) => {
 //   response.sendStatus(200).send("running post request");
 
-  //   console.log("called1");
-  //   let body_param = request.body;
+//   console.log("called1");
+//   let body_param = request.body;
 
-  // process.stderr.write("request is post" + request);
+// process.stderr.write("request is post" + request);
 
-  //   // console.log("request is post" + request);
-  //   console.log(JSON.stringify(body_param, null, 2));
+//   // console.log("request is post" + request);
+//   console.log(JSON.stringify(body_param, null, 2));
 
-  //   if (body_param.object) {
-  //     if (
-  //       body_param.entry &&
-  //       body_param.entry[0].changes &&
-  //       body_param.entry[0].changes[0].value.messages &&
-  //       body_param.entry[0].changes[0].value.messages[0]
-  //     ) {
-  //       let phone_no_id =
-  //         body_param.entry[0].changes[0].value.metadata.phone_number_id;
-  //       let from = body_param.entry[0].changes[0].value.messages[0].from;
-  //       let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+//   if (body_param.object) {
+//     if (
+//       body_param.entry &&
+//       body_param.entry[0].changes &&
+//       body_param.entry[0].changes[0].value.messages &&
+//       body_param.entry[0].changes[0].value.messages[0]
+//     ) {
+//       let phone_no_id =
+//         body_param.entry[0].changes[0].value.metadata.phone_number_id;
+//       let from = body_param.entry[0].changes[0].value.messages[0].from;
+//       let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
 
-  //       axios({
-  //         method: post,
-  //         url:
-  //           "https://graph.facebook.com/v18.0/" +
-  //           phone_no_id +
-  //           "/messages?access_token=" +
-  //           token,
-  //         data: {
-  //           messaging_product: "whatsapp",
-  //           to: from,
-  //           text: {
-  //             body: "Hi!!!!!!!!!!!!!! I am Dhruv Gupta",
-  //           },
-  //         },
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //         },
-  //       });
-  //       response.status(200);
-  //     } else {
-  //       response.status(404);
-  //     }
-  //   }
+//       axios({
+//         method: post,
+//         url:
+//           "https://graph.facebook.com/v18.0/" +
+//           phone_no_id +
+//           "/messages?access_token=" +
+//           token,
+//         data: {
+//           messaging_product: "whatsapp",
+//           to: from,
+//           text: {
+//             body: "Hi!!!!!!!!!!!!!! I am Dhruv Gupta",
+//           },
+//         },
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       });
+//       response.status(200);
+//     } else {
+//       response.status(404);
+//     }
+//   }
 // });
-
-
 
 // https://graph.facebook.com/v18.0/224738690728648/messages `
 //   -H 'Authorization: Bearer EAAP6A94pp78BOwRyZBZCIKFwULdLKe6scqUxuBgOvD6bwPifOMctVNcUTKRPOfLjTcZBvfI6M8CFJcwAA9KU59JHtrcBhITaE0cCnWbJcqSykW0QW5eulGdAb5BLU4et812ZCZC4skKhBSSanqNOmsBRDSniBmEOv60f5a8ttyfl4s9COrGT9RBLb51I62xymZA5SjjOtZAraU39iNpbykZD' `
